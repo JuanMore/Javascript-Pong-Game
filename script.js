cb5de4d4ac9379a68feb0691c5f01eeb73862680
Juan Moreno <morenostudent1@gmail.com>
2021-12-12 13:47:04 -0800

    Update script.js

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function handleLose() {
   computerPaddle.reset()
 }
 
- document.addEventListener("touchmove", e => {
+ document.addEventListener("mousemove", e => {
     // convert it to value between 0-100
   playerPaddle.position = (e.y / window.innerHeight) * 100
 })
